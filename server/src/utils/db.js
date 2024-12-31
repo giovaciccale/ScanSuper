13de9cad6b96857c7132da4f05751a94b648b22c
giovaciccale <giovaciccale@hotmail.com>
2024-12-31 15:22:19 -0300

    Actualiza conexión a MongoDB y elimina warnings

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -2,10 +2,12 @@ import { connect } from "mongoose";
 
 const dbConnection = async () => {
   try {
-    await connect(process.env.DB_LINK);
+    await connect("mongodb+srv://giovaciccale:45ZgrOpY4dAr8SJR@mycluster-ciccale.tag7jqq.mongodb.net/super?retryWrites=true&w=majority", {
+      serverSelectionTimeoutMS: 5000, // Opcional, pero puedes mantenerlo si quieres
+    });
     console.log("db mongo connected");
   } catch (error) {
-    console.log(error);
+    console.error("Error al conectar a MongoDB:", error.message);
   }
 };
 
